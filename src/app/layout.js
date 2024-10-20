65ab4f557546dbebfa52e7f36ad09c749b16ef1f
yudhistiaR <Yudhistia110@gmail.com>
2024-10-20 11:23:50 +0800

    Change: renew bottom navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,24 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import { config } from "@/lib/config";
+import localFont from 'next/font/local';
+import './globals.css';
+import { config } from '@/lib/config';
+import Navbar from '@/components/navbar/Navbar';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900'
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900'
 });
 
 export const metadata = {
   title: config.nama,
-  author: "Udin",
+  author: 'FTI UNISKA',
   description:
-    "Aplikasi Pemilihan Ketua Umum HMP dan Pemilihan Gubernur BEM FTI UNISKA",
+    'Aplikasi Pemilihan Ketua Umum HMP dan Pemilihan Gubernur BEM FTI UNISKA'
 };
 
 export default function RootLayout({ children }) {
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Navbar />
       </body>
     </html>
   );
